e3d7e8393ce9425d619eefdc579168d239b31c65
Gauraw Singh <singh.gauraw@gmail.com>
2018-08-28 18:21:53 +0530

    added a statement in feature2

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     class Feature1
     {
         //Change in Feature branch
-
+        //Going to call feature 2
         public int sum(int x,int y)
         {
           return x + y;
