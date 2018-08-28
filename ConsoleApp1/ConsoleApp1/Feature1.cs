235cfa806916871e8bb13487d80d89a7ba93256f
Gauraw Singh <singh.gauraw@gmail.com>
2018-08-28 18:29:23 +0530

    added feature 3

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     class Feature1
     {
         //Change in Feature branch
-
+        // need to call feature 3
         public int sum(int x,int y)
         {
           return x + y;
